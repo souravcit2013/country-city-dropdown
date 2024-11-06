d66b2cb1174c56bf356e175f120807ac407032da
Sourav Sarkar <75407895+souravcit2013@users.noreply.github.com>
2024-11-06 03:12:03 +0000

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Country from "./Components/dropDown";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Country />
     </div>
   );
 }
---
+++ src/Components/dropDown.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const Country = () => {
+  return (
+    <>
+      <div>
+        <label>Country</label>
+        <select></select>
+      </div>
+    </>
+  );
+};
+
+export default Country;
